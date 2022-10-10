e2d94f0ba6530a2ce0a9e08ef3b1ddcf275124f0
DimaKuzmin <scorpion26-10-96@yandex.ua>
2022-10-10 17:42:20 +0300

    Переместил папку проекта

--- SDK/include/lua/library_linkage.h
+++ SDK/include/lua/library_linkage.h
@@ -8,8 +8,8 @@
 
 #pragma once
 
-#ifndef LUAJIT_2
-	#pragma comment(lib, "lua.JIT.1.1.4.lib" )
-#else
-	#pragma comment (lib, "lua51.lib")
-#endif
+ 
+//	#pragma comment(lib, "lua.JIT.1.1.4.lib" )
+ 
+#pragma comment (lib, "lua51.lib")
+ 
