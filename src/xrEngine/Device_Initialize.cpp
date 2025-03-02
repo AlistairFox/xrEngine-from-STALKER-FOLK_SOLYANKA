8afe5ce6d384253b98a0a9c210e874838636627d
DimaKuzmin <scorpion261096@gmail.com>
2025-03-03 00:11:21 +0200

    Update Device_Initialize.cpp

--- src/xrEngine/Device_Initialize.cpp
+++ src/xrEngine/Device_Initialize.cpp
@@ -78,7 +78,7 @@ PROTECT_API void CRenderDevice::Initialize			()
 
         // Create the render window
 		m_hWnd = CreateWindowEx( WS_EX_TOPMOST, 
-								wndclass, "S.T.A.L.K.E.R.: Call of Pripyat", m_dwWindowStyle,
+								wndclass, "X-Ray OMP (Folk Solyanka)", m_dwWindowStyle,
                                /*rc.left, rc.top, */CW_USEDEFAULT, CW_USEDEFAULT,
                                (rc.right-rc.left), (rc.bottom-rc.top), 0L,
                                0, hInstance, 0L );
