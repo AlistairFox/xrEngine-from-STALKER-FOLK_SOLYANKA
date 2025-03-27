fafe668b3ab580a24379a85ca0ab4d2a174da752
DriveAlphinist <drive1402@gmail.com>
2025-03-27 13:11:18 +0500

    [Weapon] Фикс условия в XFORM
    баговался бинокль и нож

--- src/xrGame/Weapon_XFORM.cpp
+++ src/xrGame/Weapon_XFORM.cpp
@@ -241,9 +241,9 @@ void CWeapon::UpdateXForm()
 		mRes.mulA_43(E->XFORM());
 	}
 
-	if (CurrSlot() == INV_SLOT_3)
+	if (!(CurrSlot() == INV_SLOT_2))
 		UpdatePosition(mRes);
-	else if (CurrSlot() == INV_SLOT_2)
+	else 
 		UpdatePosition_alt(mRes);
 }
 
